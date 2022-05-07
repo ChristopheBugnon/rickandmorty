869565f7394429f1d1c25a36d24d6c1b1726e261
Christophe Bugnon <christophebugnon@MacBook-Pro-de-Christophe.local>
2022-05-07 14:33:01 +0200

    Create FeedItem object

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Christophe Bugnon on 07/05/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: Int
+    let name: String
+    let status: String
+    let species: String
+    let origin: String
+    let imageURL: URL
+}
